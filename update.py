ee6c8e18807b1211ead9520eb03d47e72d8379da
Brandon Parsons <parsons.brandon@gmail.com>
2014-07-13 12:15:14 -0600

    Need to convert UUID to string first

--- update.py
+++ update.py
@@ -55,7 +55,7 @@ mean_returns, std_dev_returns, covariance_matrix = stats.generate_stats(prices)
 etfs = load_json_data.get_etfs()
 
 def uuid_for(ticker):
-    return uuid.uuid3(uuid.NAMESPACE_OID, ticker)
+    return str(uuid.uuid3(uuid.NAMESPACE_OID, ticker))
 
 def add_id_to_etf(etf):
     etf['id'] = uuid_for(etf['ticker'])
